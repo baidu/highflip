fb10e4e1653093c0154217c01cdd30c82ebf16a0
chenzhiyu <chenzhiyu@baidu.com>
2022-09-20 17:33:01 +0800

    fix imports BFL-1784 Change-Id: Iced34a8734d9173ce45fe65ec4a7dbe532060af9
    Change-Id: I30892f88afe02857aa876cf68740f61fe3bb1900

--- highflip-core/src/main/java/com/baidu/highflip/core/engine/adaptor/PartnerAdaptor.java
+++ highflip-core/src/main/java/com/baidu/highflip/core/engine/adaptor/PartnerAdaptor.java
@@ -2,8 +2,6 @@ package com.baidu.highflip.core.engine.adaptor;
 
 import com.baidu.highflip.core.entity.runtime.Partner;
 
-import java.util.Iterator;
-
 public interface PartnerAdaptor {
 
     Partner getPartner(Partner partner);
--- highflip-core/src/main/java/com/baidu/highflip/core/entity/runtime/Job.java
+++ highflip-core/src/main/java/com/baidu/highflip/core/entity/runtime/Job.java
@@ -11,17 +11,13 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EntityListeners;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import java.time.LocalDateTime;
-import java.util.List;
 import java.util.Map;
 
 @Data
