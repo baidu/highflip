c845d9102bb5165dccfbdf5e81554f4c2970b882
chenzhiyu <chenzhiyu@baidu.com>
2022-09-22 11:15:12 +0800

    format codes BFL-1784 Change-Id: Iced34a8734d9173ce45fe65ec4a7dbe532060af9
    Change-Id: Iba19d2edf7da2227a2f37c8e384f5585a24aa020

--- highflip-vendors/highflip-adapter-dumb/src/main/java/com/baidu/highflip/adaptor/dumb/DumbTaskAdaptor.java
+++ highflip-vendors/highflip-adapter-dumb/src/main/java/com/baidu/highflip/adaptor/dumb/DumbTaskAdaptor.java
@@ -12,30 +12,35 @@ import java.util.Iterator;
 @Component
 public class DumbTaskAdaptor implements TaskAdaptor {
 
+    @Override
     public Task getTask(Task task) {
         return null;
     }
 
+    @Override
     public void deleteTask(Task task) {
 
     }
-
+    @Override
     public boolean hasTask(Task task) {
         return false;
     }
 
+    @Override
     public int getTaskLogCount() {
         return 0;
     }
 
+    @Override
     public Iterator<String> getTaskLog(Task task, int offset, int limit) {
         return null;
     }
 
+    @Override
     public void controlTask(Task task, Action action) {
 
     }
-
+    @Override
     public void invokeTask(Task task) {
 
     }
