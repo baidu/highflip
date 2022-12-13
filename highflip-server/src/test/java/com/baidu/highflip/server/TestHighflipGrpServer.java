84b2c11ae6d50d9418431f8131f17ce12aecc7fd
chen.zhiyu <quby@sina.com>
2022-12-13 18:13:04 +0800

    add key index to object

--- highflip-server/src/test/java/com/baidu/highflip/server/TestHighflipGrpServer.java
+++ highflip-server/src/test/java/com/baidu/highflip/server/TestHighflipGrpServer.java
@@ -2,8 +2,10 @@ package com.baidu.highflip.server;
 
 import com.baidu.highflip.server.common.GrpcServerTestBase;
 import com.google.common.collect.Streams;
+import highflip.HighflipMeta;
 import highflip.v1.HighFlipGrpc;
 import highflip.v1.Highflip;
+import io.grpc.stub.StreamObserver;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -19,6 +21,10 @@ public class TestHighflipGrpServer extends GrpcServerTestBase {
 
     HighFlipGrpc.HighFlipBlockingStub getStub() {
         return HighFlipGrpc.newBlockingStub(getChannel());
+    }
+
+    HighFlipGrpc.HighFlipStub getRawStub() {
+        return HighFlipGrpc.newStub(getChannel());
     }
 
     @Test
@@ -45,6 +51,25 @@ public class TestHighflipGrpServer extends GrpcServerTestBase {
         for (String name : result) {
             log.info("config name: {}", name);
         }
+    }
+
+    @Test
+    public void testCreateJob() {
+        HighflipMeta.GraphProto graph = HighflipMeta.GraphProto
+                .newBuilder()
+                .setName("test_graph")
+                .build();
+
+        Highflip.JobCreateRequest request = Highflip.JobCreateRequest
+                .newBuilder()
+                .setName("test")
+                .setDag(graph)
+                .build();
+
+        highflip.v1.Highflip.JobId response = getStub()
+                .createJob(request);
+
+        log.info("create job id: {}", response.getJobId());
     }
 
     @Test
@@ -79,6 +104,53 @@ public class TestHighflipGrpServer extends GrpcServerTestBase {
         for (String taskId : result) {
             log.info("task id: {}", taskId);
         }
+    }
+
+    @Test
+    public void testPushData() {
+
+
+        StreamObserver<Highflip.DataPushRequest> requests = getRawStub()
+                .pushData(new StreamObserver<>() {
+
+                    @Override
+                    public void onNext(Highflip.DataId value) {
+                        log.info("push data id: {}", value.getDataId());
+                    }
+
+                    @Override
+                    public void onError(Throwable t) {
+
+                    }
+
+                    @Override
+                    public void onCompleted() {
+
+                    }
+                });
+
+        Highflip.DataPushRequest head = Highflip.DataPushRequest
+                .newBuilder()
+                .setHead(Highflip.DataPushRequest.Head
+                        .newBuilder()
+                        .setMode(Highflip.DataMode.DENSE)
+                        .build())
+                .build();
+        requests.onNext(head);
+
+        Highflip.DataPushRequest body = Highflip.DataPushRequest
+                .newBuilder()
+                .setDense(Highflip.DenseData
+                        .newBuilder()
+                        .addRows(Highflip.DenseData.Row
+                                .newBuilder()
+                                .addAllValue(List.of("1", "2", "3"))
+                                .build())
+                        .build())
+                .build();
+        requests.onNext(body);
+
+        requests.onCompleted();
     }
 
     @Test
