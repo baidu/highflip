595e7e33ec94fd18ee036c55cccf9ea76d711c29
TingSty <615874106@qq.com>
2023-04-27 11:27:24 +0800

    enable job DAG save and retrieve

--- highflip-editor/src/main/java/com/baidu/highflip/editor/web/controller/HighFlipController.java
+++ highflip-editor/src/main/java/com/baidu/highflip/editor/web/controller/HighFlipController.java
@@ -2,6 +2,8 @@ package com.baidu.highflip.editor.web.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,9 +11,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.baidu.highflip.editor.model.Result;
+import com.baidu.highflip.editor.utils.StringUtils;
 import com.baidu.highflip.editor.vo.ListDataRequest;
 import com.baidu.highflip.editor.vo.ListOperatorsRequest;
 import com.baidu.highflip.editor.vo.ListPartnersRequest;
+import com.baidu.highflip.editor.vo.SaveJobRequest;
 import com.baidu.highflip.editor.web.services.HighFlipEditorService;
 import com.baidu.highflip.editor.vo.LoginRequest;
 import com.baidu.highflip.editor.web.annotation.IgnoreRequestBody;
@@ -19,13 +23,14 @@ import com.baidu.highflip.editor.web.annotation.IgnoreRequestBody;
 import highflip.v1.Highflip;
 
 @RestController
+@RequestMapping(value = "highflip")
 public class HighFlipController {
 
     @Autowired
     HighFlipEditorService highFlipEditorService;
 
     @IgnoreRequestBody
-    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    @RequestMapping(value = "login", method = RequestMethod.POST)
     public Result login(@RequestBody LoginRequest request) {
         highFlipEditorService.login(request.getUsername(),
                                     request.getPassword(),
@@ -73,4 +78,26 @@ public class HighFlipController {
                         username, password, request.getPartnerId());
         return Result.success(dataGetResponseList);
     }
+
+    @RequestMapping(value = "job", method = RequestMethod.POST)
+    public Result saveJob(HttpServletRequest request,
+                          @RequestBody SaveJobRequest saveJobRequest) {
+        String token = request.getHeader("TOKEN");
+        if (StringUtils.isBlank(token)) {
+            return Result.failure(400, "The required header TOKEN has not found");
+        }
+        highFlipEditorService.saveJob(token, saveJobRequest.getJson());
+        return Result.success();
+    }
+
+    @RequestMapping(value = "job", method = RequestMethod.GET)
+    public Result getJob(HttpServletRequest request) {
+        String token = request.getHeader("TOKEN");
+        if (StringUtils.isBlank(token)) {
+            return Result.failure(400, "The required header TOKEN has not found");
+        }
+        String jobDag = highFlipEditorService.getJob(token);
+        return Result.success(jobDag);
+    }
+
 }
